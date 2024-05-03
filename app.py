42ad3c000ae7bf919a617bf0c6ea6999ec3953ad
giordan <giordanpretlein@gmail.com>
2024-05-03 11:44:31 +0100

    update

--- app.py
+++ app.py
@@ -3,6 +3,20 @@ import joblib
 import traceback
 import pandas as pd
 import numpy as np
+import json
+
+
+class NumpyEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, np.integer):
+            return int(obj)
+        elif isinstance(obj, np.floating):
+            return float(obj)
+        elif isinstance(obj, np.ndarray):
+            return obj.tolist()
+        else:
+            return super(NumpyEncoder, self).default(obj)
+
 
 app = Flask(__name__)
 
@@ -12,22 +26,23 @@ def predict():
     if lr:
         try:
             json_ = request.json
+            print(json_)
             query = pd.get_dummies(pd.DataFrame(json_))
+
+            query_dict = query.to_dict(orient="records")
+            query = pd.get_dummies(pd.DataFrame(query_dict))
+
             query = query.reindex(columns=model_columns, fill_value=0)
 
             prediction = lr.predict(query)
+            response = json.dumps({"prediction": prediction.tolist()}, cls=NumpyEncoder)
 
-            return jsonify({"prediction": list(prediction)})
+            return response
         except:
             return jsonify({"trace": traceback.format_exc()})
     else:
         print("Train the model first")
         return "No model here to use"
-
-
-@app.route("/", methods=["POST", "GET"])
-def welcome():
-    return "Welcome to the api!"
 
 
 if __name__ == "__main__":
